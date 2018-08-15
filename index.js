f22e965c539d2c76671a2282500bc3656afa20c4
Marisa Canan-Zucker <mcananzucker@gmail.com>
2018-08-15 22:05:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
 
-var lowercase = string.toLowerCase
-var uppercase = string.toUpperCase
-
 if (string === lowercase) {
-  return "I can't hear you!"
+  return `I can't hear you!`
 }else if (string === uppercase) {
-  return "YES INDEED!"
+  return `YES INDEED!`
 }else if (string === "I love you, Grandma.")  {
   return "I love you, too."
 }
